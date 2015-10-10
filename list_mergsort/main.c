88af95fa3f4e55c21bdda80fdc4ee26649a2a016
Lingyu Li <lilingyu1991@foxmail.com>
2015-10-10 10:08:55 +0800

    Initial Commit

---
+++ list_mergsort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  list_mergsort
+//
+//  Created by LiLingyu on 15/10/10.
+//  Copyright © 2015年 LiLingyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
